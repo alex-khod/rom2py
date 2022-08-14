1c7d866025a67ad97a59a534e5f6ee0a2e80fee3
alex-khod <dead7simple@gmail.com>
2022-08-14 13:17:02 +0800

    Refactor terrain stripe -> terrain column.

--- src/terrain/renderers/_textured_geom.py
+++ src/terrain/renderers/_textured_geom.py
@@ -8,9 +8,9 @@ def blit_terrain_to_texture(self):
     border = 0
     for j in range(alm.height):
         for i in range(alm.width):
-            stripe_id, row_id = alm.tilecoords[j][i]
-            stripe = self.stripes[stripe_id]
-            tile_image = stripe.get_region(border, border + i * TILE_SIZE, TILE_SIZE, TILE_SIZE)
+            column_id, row_id = alm.tilecoords[j][i]
+            column = self.columns[column_id]
+            tile_image = column.get_region(border, border + i * TILE_SIZE, TILE_SIZE, TILE_SIZE)
             x, y = i * TILE_SIZE, j * TILE_SIZE
             map_tex.blit_into(tile_image, x, y, 0)
             map_tex_tiles[j][i] = map_tex.get_region(x, y, TILE_SIZE, TILE_SIZE)
--- src/terrain/tilemap.py
+++ src/terrain/tilemap.py
@@ -10,51 +10,51 @@ class TileMap:
         Class for loading ROM2 tilemap onto texture and allowing tile-by-tile access of the texture.
     """
 
-    def __getitem__(self, stripe_id):
-        return self.tiles[stripe_id]
+    def __getitem__(self, column_id):
+        return self.tiles[column_id]
 
     def __init__(self):
-        self.stripe_order = None
-        self.stripes = None
+        self.column_order = None
+        self.columns = None
         self.texture = None
         self.tiles = None
 
-        self.load_stripes()
+        self.load_columns()
         self.blit_tiles_to_texture()
 
-    def load_stripes(self):
-        terrain_stripe_list = Resources["graphics", "terrain"]
-        self.stripe_order = [x.record.name for x in terrain_stripe_list]
-        stripes = [x.record.content for x in terrain_stripe_list]
-        # stripes = [extrude_stripe(x, 0) for x in stripes]
+    def load_columns(self):
+        column_list = Resources["graphics", "terrain"]
+        self.column_order = [x.record.name for x in column_list]
+        columns = [x.record.content for x in column_list]
+        # columns = [extrude_column(x, 0) for x in columns]
         # add brightness
-        # stripes = [x + 8 for x in stripes]
-        stripes = [np.array(x.convert("RGB")) for x in stripes]
-        stripes = [[x.shape[1], x.shape[0], x.tobytes()] for x in stripes]
-        stripes = [pyglet.image.ImageData(width=x[0], height=x[1], fmt="RGB", data=x[2]) for x in stripes]
-        self.stripes = stripes
+        # columns = [x + 8 for x in columns]
+        columns = [np.array(x.convert("RGB")) for x in columns]
+        columns = [[x.shape[1], x.shape[0], x.tobytes()] for x in columns]
+        columns = [pyglet.image.ImageData(width=x[0], height=x[1], fmt="RGB", data=x[2]) for x in columns]
+        self.columns = columns
 
     def blit_tiles_to_texture(self):
-        stripes = self.stripes
+        columns = self.columns
         border = 0
         bordered_ts = TILE_SIZE + border * 2
-        # tex_w, tex_h = len(stripes) * bordered_ts, max([x.height for x in stripes])
+        # tex_w, tex_h = len(columns) * bordered_ts, max([x.height for x in columns])
         self.texture = pyglet.image.Texture.create(2048, 2048)
         self.tiles = []
-        for i, stripe in enumerate(stripes):
-            stripe_tiles = []
-            self.texture.blit_into(stripe, bordered_ts * i, 0, 0)
-            for j in range(stripe.height // bordered_ts):
+        for i, column in enumerate(columns):
+            column_tiles = []
+            self.texture.blit_into(column, bordered_ts * i, 0, 0)
+            for j in range(column.height // bordered_ts):
                 tile_tex = self.texture.get_region(x=border + i * bordered_ts,
                                                    y=border + j * bordered_ts,
                                                    width=TILE_SIZE,
                                                    height=TILE_SIZE)
-                stripe_tiles.append(tile_tex)
-            self.tiles.append(stripe_tiles)
+                column_tiles.append(tile_tex)
+            self.tiles.append(column_tiles)
 
-def extrude_stripe(stripe, border=1):
+def extrude_column(column, border=1):
     """
-        Take PILImage :stripe with (height=*, width=TILE_SIZE) and
+        Take PILImage :column with (height=*, width=TILE_SIZE) and
         extrude it's border pixels by :border in four directions.
         :return mumpy array.
     """
@@ -62,16 +62,16 @@ def extrude_stripe(stripe, border=1):
     b, ts = border, TILE_SIZE
     tsb, w = ts + b, ts + b * 2
 
-    arr = np.array(stripe.convert("RGB"))
+    arr = np.array(column.convert("RGB"))
     h = arr.shape[0]
-    stripe_buff = np.zeros((w * h // ts, w, 3), dtype='uint8')
-    buff = np.zeros((w, w, 3), dtype='uint8')
+    column_buffer = np.zeros((w * h // ts, w, 3), dtype='uint8')
+    buffer = np.zeros((w, w, 3), dtype='uint8')
     for j in range(h // TILE_SIZE):
-        buff[b:tsb, b:tsb] = arr[j * ts:(j + 1) * ts]
-        buff[0:b, b:tsb] = arr[j * ts:j * ts + 1]
-        buff[tsb:w, b:tsb] = arr[(j + 1) * ts - 1:(j + 1) * ts]
-        buff[b:tsb, 0:b] = arr[j * ts:(j + 1) * ts, 0:1]
-        buff[b:tsb, tsb:w] = arr[j * ts:(j + 1) * ts, ts - 1:ts]
+        buffer[b:tsb, b:tsb] = arr[j * ts:(j + 1) * ts]
+        buffer[0:b, b:tsb] = arr[j * ts:j * ts + 1]
+        buffer[tsb:w, b:tsb] = arr[(j + 1) * ts - 1:(j + 1) * ts]
+        buffer[b:tsb, 0:b] = arr[j * ts:(j + 1) * ts, 0:1]
+        buffer[b:tsb, tsb:w] = arr[j * ts:(j + 1) * ts, ts - 1:ts]
 
-        stripe_buff[j * w:(j + 1) * w] = buff
-    return stripe_buff
+        column_buffer[j * w:(j + 1) * w] = buffer
+    return column_buffer
