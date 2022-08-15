889ea5ec1017e3b533cb02b7a58fec96d380b808
alex-khod <dead7simple@gmail.com>
2022-08-15 18:14:53 +0800

    Fix broken tests

--- tests/test_alm.py
+++ tests/test_alm.py
@@ -10,7 +10,7 @@ class TestAlm(TestCase):
         start = alm.width
         assert alm.heights[1][:5] == alm["heights"].body.heights[start:start + 5]
 
-    def test_heights_for_tile(self):
+    def test_tile_corner_heights_at(self):
         alm = Resources.from_file("data", "atest.alm")
         heights = alm["heights"].body.heights
 
@@ -21,15 +21,15 @@ class TestAlm(TestCase):
                 h10 = heights[j * w + i + 1]
                 h11 = heights[(j + 1) * w + i + 1]
                 h01 = heights[(j + 1) * w + i]
-                assert [h00, h10, h11, h01] == alm.heights_for_tile(i, j)
+                assert [h00, h10, h11, h01] == alm.tile_corner_heights_at(i, j)
 
-    def test_heights_for_column(self):
+    def test_heights_column_at(self):
         alm = Resources.from_file("data", "atest.alm")
         h, w = 4, 4
         alm.general_map_info.width = w
         alm.general_map_info.height = h
         alm["heights"].body.heights = sum([[0, 0, 0, 1] for _ in range(h)], [])
-        assert alm.heights_for_column(w - 1) == [1, 1, 1, 1]
+        assert alm.heights_column_at(w - 1) == (1, 1, 1, 1)
 
     def test_tilecoords_are_cached(self):
         alm = Resources.from_file("data", "atest.alm")
