0d83fd2ac0d13a60f7fc08941634d8093037036a
alex-khod <dead7simple@gmail.com>
2022-08-22 17:18:40 +0800

    some finding from exploring the resources

---
+++ tests/test_graphics_res_content.py
@@ -0,0 +1,34 @@
+from . import TestCase
+from src.resources import Resources
+
+
+class TestGraphics(TestCase):
+
+    def test_sprites_are_one_frame(self):
+        # show that there is no extra data
+        item_sprites = Resources["graphics", "inventory"]
+        for hdr in item_sprites:
+            assert hdr.record.content.count == 1
+
+        classes = ["ffighter", "fmage", "mfighter", "mmage"]
+        for klass in classes:
+            for hdr in Resources["graphics", "equipment", klass]:
+                if hdr.record.name in ["primary", "secondary"]:
+                    continue
+                if len(hdr.record.bytes) == 0:
+                    continue
+                sprite = hdr.record.content
+                assert sprite.count == 1
+
+            for wielding in ["primary", "secondary"]:
+                for hdr in Resources["graphics", "equipment", klass, wielding]:
+                    if len(hdr.record.bytes) == 0:
+                        # skip faulty zero-length images in .RES
+                        continue
+                    sprite = hdr.record.content
+                    assert sprite.count == 1
+
+    def test_count_backpack(self):
+        backpack = Resources["graphics", "backpack", "sprites.256"].content
+        # this is a sack sprite with 6 various frames for each
+        assert len(backpack) == 6
