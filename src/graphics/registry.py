bb174fafcee62bac89ffd5279d7006e84153856d
alex-khod <dead7simple@gmail.com>
2022-08-26 05:00:43 +0800

    png support / some images

--- src/graphics/registry.py
+++ src/graphics/registry.py
@@ -1,5 +1,6 @@
 import os.path
 
+import pyglet.image
 from profilehooks import timecall, profile
 
 from src.graphics.textures import TextureAtlas, TextureBin, Texture
@@ -58,8 +59,9 @@ class GraphicsRegistry:
         graphics_res = Resources["graphics"]
         files = graphics_res.list_files()
         whitelist = ["objects", "units", "structures"]
+        blacklist = ["spritesb.256", "houseb.256"]
         files = filter(lambda x: any([x.startswith(word) for word in whitelist]), files)
-        files = filter(lambda x: not x.endswith("spritesb.256"), files)
+        files = filter(lambda x: not any([x.endswith(word) for word in blacklist]), files)
         ext_keys = list(self.ext_mapping.keys())
         files = filter(lambda x: any([x.endswith(ext) for ext in ext_keys]), files)
 
@@ -69,11 +71,29 @@ class GraphicsRegistry:
             if len(graphics_res[path].bytes) == 0:
                 print(f"Zero-length sprite: {path}")
                 continue
-            content = graphics_res[path].content
+            try:
+                content = graphics_res[path].content
+            except Exception as e:
+                print(e)
             store_action(content, path.lower())
             # import traceback
             # traceback.print_exc()
             # print(f"Error loading: graphics.res @ {path}")
+        png = Resources.from_file("data", "RAR_Heavy_Tank_Ingame0.png")
+        self.prepare_png(png, "tank.png")
+        light = Resources["graphics", "projectiles", "lightnin", "sprites.16a"].content
+        self.prepare_rom16a(light, "lighting")
+        fireball = Resources["graphics", "projectiles", "fireball", "sprites.16a"].content
+        self.prepare_rom16a(fireball, "fireball")
+        teleport = Resources["graphics", "projectiles", "teleport", "sprites.16a"].content
+        self.prepare_rom16a(teleport, "teleport")
+        explosion = Resources["graphics", "projectiles", "fireexpl", "sprites.16a"].content
+        self.prepare_rom16a(explosion, "explosion")
+
+        proj1 = Resources["graphics", "projectiles", "projectiles.pal"].content
+        self.prepare_palette(proj1, "projpal")
+        proj2 = Resources["graphics", "projectiles", "projectile_.pal"].content
+        self.prepare_palette(proj2, "projpal2")
 
     def __getitem__(self, item):
         return self._items[item]
@@ -96,9 +116,18 @@ class GraphicsRegistry:
         self._items[sprite_key] = textures
 
     def prepare_rom16a(self, rom16a, sprite_key):
+        if rom16a.has_palette:
+            self.prepare_palette(rom16a.palette, sprite_key + "inner")
         textures = []
         for image in rom16a:
             texture = image.to_rg_image_data()
             texture_region = self.rom16a_atlas.add(texture)
             textures.append(texture_region)
         self._items[sprite_key] = textures
+
+    def prepare_png(self, png, sprite_key):
+        w, h = png.width, png.height
+        data = png.convert("RGBA").tobytes()
+        imdata = pyglet.image.ImageData(width=w, height=h, fmt="RGBA", data=data)
+        texture = imdata.create_texture(pyglet.image.Texture)
+        self._items[sprite_key] = texture
